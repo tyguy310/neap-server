3305f45e64cc27213450614f660d19505506b39c
Tyler Maier <tyler_maier@yahoo.com>
2016-10-24 11:00:14 -0600

    heroku

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const databaseName = 'coffee_db';
 module.exports = {
   production: {
     client: 'postgresql',
-    connection: `postgres://sxnaqdzocquuzc:e3qg0URRRfB7SqHaOJPeJ6Pj17@ec2-54-83-31-65.compute-1.amazonaws.com:5432/d3h7l2jdsjpvvh/${databaseName}`,
+    connection: `postgres://sxnaqdzocquuzc:e3qg0URRRfB7SqHaOJPeJ6Pj17@ec2-54-83-31-65.compute-1.amazonaws.com:5432/d3h7l2jdsjpvvh`,
     migrations: {
       directory: __dirname + '/src/server/db/migrations'
     },
