a5e4ca6523e7b98b9522e8a245685758d08265ac
Tyler Maier <tyler_maier@yahoo.com>
2016-10-25 09:18:17 -0600

    post route

--- src/server/routes/coffee.js
+++ src/server/routes/coffee.js
@@ -1,30 +1,40 @@
 const express = require('express');
 const router = express.Router();
+
 const knex = require('../db/connection');
 
-router.get('/', function (req, res, next) {
-  return knex('coffee')
-  .then(coffees => {
+router.get('/', (req, res, next) => {
+  return knex('coffee').select('*')
+  .then((coffees) => {
     res.status(200).json({
       status: 'success',
       data: coffees
     });
   })
-  .catch(err => next(err));
+  .catch((err) => { return next(err); });
 });
 
-router.get('/:id', function (req, res, next) {
-  const id = req.params.id;
-
-  return knex('coffee')
-  .where('id', id)
-  .then(coffee => {
+router.get('/:id', (req, res, next) => {
+  const coffeeID = parseInt(req.params.id);
+  return knex('coffee').where('id', coffeeID).first()
+  .then((coffee) => {
     res.status(200).json({
       status: 'success',
       data: coffee
     });
   })
-  .catch(err => next(err));
+  .catch((err) => { return next(err); });
+});
+
+router.post('/', (req, res, next) => {
+  return knex('coffee').insert(req.body).returning('*')
+  .then((coffee) => {
+    res.status(200).json({
+      status: 'success',
+      data: coffee
+    });
+  })
+  .catch((err) => { return next(err); });
 });
 
 module.exports = router;
